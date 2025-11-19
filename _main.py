6e27877a61d346b294d69c3bc61f53df7cc47f5b
twsec <twsec@nyist.edu.cn>
2025-11-19 18:12:57 +0800

    3

--- _main.py
+++ _main.py
@@ -1,251 +1,154 @@
-from __future__ import print_function
+import argparse
 
-import torch
-import torch.nn.functional as F
-import torch.optim as optim
-from tensorboardX import SummaryWriter
-import os
-import logging
-from datetime import datetime
-
-from clients_attackers import *
-from server import Server
+import numpy as np
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", help='Default=64', type=int, default=64)
+    parser.add_argument("--test_batch_size", help='Default=64',type=int, default=64)
+    parser.add_argument("--epochs", type=int, default=10,
+        help='Default is 10 epochs.')
+    parser.add_argument("--optimizer", type=str, default='SGD',
+        help='Default is SGD optimizer')
+    parser.add_argument("--lr", type=float, default=0.01,
+        help="Learning rate of models. Default=0.01.")
+    parser.add_argument("--momentum", type=float, default=0.5,
+        help="Default momentum is 0.5.")
+    parser.add_argument("--weight_decay", type=float, default=0.,
+        help="Default weight_decay is 0.")
+    parser.add_argument("--seed", type=int, default=1,
+        help='Default is 1.')
+    parser.add_argument("--log_interval", type=int, default=11,
+        help='Default log_interval is 11.')
+    parser.add_argument("-n", "--num_clients", type=int, default=10,
+        help='Default is 10 clients.')
+    parser.add_argument("--output_folder", type=str, default="experiments",
+                        help="path to output folder, e.g. \"experiment\"")
+    parser.add_argument("--dataset", type=str,
+        choices=["mnist", "cifar", "cifar100", "imdb", "fashion_mnist"],
+        default="mnist", help="Default is mnist dataset.")
+    parser.add_argument("--loader_type", type=str,
+        choices=["iid", "byLabel", "dirichlet"], default="iid",
+        help="Default is iid data distribution.")
+    parser.add_argument("--loader_path", type=str, default="./data/loader.pk",
+        help="where to save the data partitions. Default is ./data/loader.pk")
+    parser.add_argument("--AR", type=str, default='fedavg',
+        choices=['fedavg', 'median', 'gm', 'krum', 'mkrum', 'foolsgold',
+            'residualbase', 'attention', 'mlp', 'mudhog', 'fedavg_oracle'],
+        help="Aggregation rule. Default is fedavg.")
+    parser.add_argument("--n_attacker_backdoor", type=int, default=0,
+        help="Default is no attacker backdoor.")
+    parser.add_argument("--backdoor_trigger", nargs="*",  type=int,
+        default=[0,0,1,1], help="the hyperparameter for backdoor trigger, do `--backdoor_trigger x_offset y_offset x_interval y_interval`")
+    parser.add_argument("--n_attacker_semanticBackdoor", type=int, default=0)
+    parser.add_argument("--n_attacker_labelFlipping", type=int, default=0)
+    parser.add_argument("--n_attacker_labelFlippingDirectional", type=int, default=0)
+    parser.add_argument("--n_attacker_omniscient", type=int, default=0)
+    parser.add_argument("--omniscient_scale", type=int, default=1)
+    parser.add_argument("--attacks", type=str,
+        help="if contains \"backdoor\", activate the corresponding tests")
+    parser.add_argument("--save_model_weights", action="store_true")
+    parser.add_argument("--experiment_name", type=str)
+    parser.add_argument("--path_to_aggNet", type=str)
+    parser.add_argument("--device", type=str, choices=["cuda", "cpu"], default='cuda')
+    parser.add_argument("--inner_epochs", type=int, default=1)
+    # Mao add for MUD-HoG
+    parser.add_argument("--verbose", action="store_true")
+    #======================UNRELIABLE clients================================
+    parser.add_argument("--list_unreliable", type=str,
+        help="List of unreliable clients, separate by comma \',\', e.g., \"1,2\"."
+            " Default is None.")
+    parser.add_argument("--unreliable_fraction", type=float, default=0.5,
+        help="Fraction of local dataset is added noise to simulate unreliable"
+            " client. Default is 0.5.")
+    parser.add_argument("--unreliable_fracTrain", type=float, default=0.3,
+        help="Fraction of local dataset is used to trained the local model."
+            " Default is 0.3.")
+    parser.add_argument("--mean_unreliable", type=float, default=0.,
+        help="Mean of Gaussian noise add to raw image for unreliable clients."
+            " Default is 0")
+    parser.add_argument("--blur_method", type=str, default='gaussian_smooth',
+        choices=['add_noise', 'gaussian_smooth'],
+        help="Method to make low quality image (use std=50). Default: gaussian_smooth")
+    parser.add_argument("--max_std_unreliable", type=float, default=50,
+        help="Max Standard deviation of Gaussian noise add to raw image (below 1),"
+            " or generate weight Gaussian smooth filter (std=50)"
+            " for unreliable clients."
+            " Value in (0,1]. Default is 50 (for gaussian_smooth).")
+    #======================= ADDITIVE NOSE attacker=========================
+    parser.add_argument("--list_uatk_add_noise", type=str,
+        help="List of untargeted attacks, sending: Gaussian noise + gradient updates.")
+    parser.add_argument("--mean_add_noise", type=float, default=0.,
+        help="Mean of additive Gaussian noise add to grad for untargeted attack. Default is 0.")
+    parser.add_argument("--std_add_noise", type=float, default=0.01,
+        help="Standard deviation of additive Gaussian noise add to grad for untargeted attack."
+            " Default is 0.01.")
+    #=====================SIGN-FLIPPING Gradients attacker====================
+    parser.add_argument("--list_uatk_flip_sign", type=str,
+        help="List of untargeted attacks, sending flipping sign of gradient updates.")
 
-def main(args):
-    log_dir = f'logfiles/{args.AR}/{args.dataset}/{args.loader_type}'
-    if not os.path.isdir(log_dir):
-        os.makedirs(log_dir)
+    #=====================LABEL-FLIPPING TARGETED attacker====================
+    parser.add_argument("--list_tatk_label_flipping", type=str,
+        help="List of targeted attacks, change label from source_label(1) to target_label(7).")
+    parser.add_argument("--list_tatk_multi_label_flipping", type=str,
+        help="List of targeted attacks, change few labels from source_label(1,2,3) to target_label(7).")
+    parser.add_argument("--source_labels", type=str, default ="1",
+        help="Default of source label flipping is 1.")
+    parser.add_argument("--target_label", type=int, default =7,
+        help="Default of source label flipping is 7.")
+    parser.add_argument("--list_tatk_backdoor", type=str,
+        help="List of targeted backdoor attack, manipulate label 2 in MNIST."
+             " Same meaning as n_attacker_backdoor, but list input option.")
 
-    FORMAT = '%(asctime)-15s %(levelname)s %(filename)s %(lineno)s:: %(message)s'
-    start_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-    FILENAME = '{0}/train_{1}_{2}_{3}_{4}_{5}.log'.format(log_dir, args.AR,
-        args.dataset, args.loader_type, args.experiment_name, start_time)
-    LOG_LVL = logging.DEBUG if args.verbose else logging.INFO
+    args = parser.parse_args()
 
-    fileHandler = logging.FileHandler(FILENAME, mode='w')
-    fileHandler.setFormatter(logging.Formatter(FORMAT))
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(logging.Formatter(FORMAT))
+    n = args.num_clients
 
-    logger = logging.getLogger('')
-    if logger.hasHandlers():
-        logger.handlers.clear()
-    logger.addHandler(consoleHandler)
-    logger.addHandler(fileHandler)
-    logger.setLevel(LOG_LVL)
-    logging.info("#" * 64)
+    m = args.n_attacker_backdoor
+    args.attacker_list_backdoor = np.random.permutation(list(range(n)))[:m]
+
+    m = args.n_attacker_semanticBackdoor
+    args.attacker_list_semanticBackdoor = np.random.permutation(list(range(n)))[:m]
+
+    m = args.n_attacker_labelFlipping
+    args.attacker_list_labelFlipping = np.random.permutation(list(range(n)))[:m]
+
+    m = args.n_attacker_labelFlippingDirectional
+    args.attacker_list_labelFlippingDirectional = np.random.permutation(list(range(n)))[:m]
+
+    m = args.n_attacker_omniscient
+    args.attacker_list_omniscient = np.random.permutation(list(range(n)))[:m]
+
+    m = args.list_unreliable
+    args.list_unreliable = [int(i) for i in m.split(',')] if m else []
+    m = args.list_uatk_add_noise
+    args.list_uatk_add_noise = [int(i) for i in m.split(',')] if m else []
+    m = args.list_uatk_flip_sign
+    args.list_uatk_flip_sign = [int(i) for i in m.split(',')] if m else []
+    m = args.list_tatk_multi_label_flipping
+    args.list_tatk_multi_label_flipping = [int(i) for i in m.split(',')] if m else []
+    m = args.list_tatk_label_flipping
+    args.list_tatk_label_flipping = [int(i) for i in m.split(',')] if m else []
+    m = args.list_tatk_backdoor
+    args.list_tatk_backdoor = [int(i) for i in m.split(',')] if m else []
+    m = args.source_labels
+    args.source_labels = [int(i) for i in m.split(',')] if m else [1]
+
+    if args.experiment_name == None:
+        args.experiment_name = f"{args.loader_type}/{args.attacks}/{args.AR}"
+
+    return args
+
+
+if __name__ == "__main__":
+
+    import _main
+
+    args = parse_args()
+    print("#" * 64)
     for i in vars(args):
-        logging.info(f"#{i:>40}: {str(getattr(args, i)):<20}#")
-    logging.info("#" * 64)
-    logging.info(args)
-    logging.info('#####################')
-    logging.info('#####################')
-    logging.info('#####################')
-    logging.info(f'Aggregation Rule:\t{args.AR}')
-    logging.info(f'Data distribution:\t{args.loader_type}')
-    logging.info(f'Attacks:\t{args.attacks} ')
-    logging.info('#####################')
-    logging.info('#####################')
-    logging.info('#####################')
+        print(f"#{i:>40}: {str(getattr(args, i)):<20}#")
+    print("#" * 64)
+    _main.main(args)
 
-    torch.manual_seed(args.seed)
-
-    device = args.device
-
-    attacks = args.attacks
-
-    writer = SummaryWriter(f'./logs/{args.output_folder}/{args.experiment_name}')
-
-    if args.dataset == 'mnist':
-        from tasks import mnist
-        trainData = mnist.train_dataloader(args.num_clients,
-            loader_type=args.loader_type, path=args.loader_path, store=False)
-        testData = mnist.test_dataloader(args.test_batch_size)
-        Net = mnist.Net
-        criterion = F.cross_entropy
-    elif args.dataset == 'cifar':
-        from tasks import cifar10 as cifar
-        trainData = cifar.train_dataloader(args.num_clients,
-            loader_type=args.loader_type, path=args.loader_path, store=False)
-        testData = cifar.test_dataloader(args.test_batch_size)
-        Net = cifar.Net
-        criterion = F.cross_entropy
-    elif args.dataset == 'cifar100':
-        from tasks import cifar100
-        trainData = cifar100.train_dataloader(args.num_clients,
-            loader_type=args.loader_type, path=args.loader_path, store=False)
-        testData = cifar100.test_dataloader(args.test_batch_size)
-        Net = cifar100.Net
-        criterion = F.cross_entropy
-    elif args.dataset == 'imdb':
-        from tasks import imdb
-        trainData = imdb.train_dataloader(args.num_clients,
-            loader_type=args.loader_type, path=args.loader_path, store=False)
-        testData = imdb.test_dataloader(args.test_batch_size)
-        Net = imdb.Net
-        criterion = F.cross_entropy
-    elif args.dataset == 'fashion_mnist':
-        from tasks import fashion_mnist
-        trainData = fashion_mnist.train_dataloader(args.num_clients,
-            loader_type=args.loader_type, path=args.loader_path, store=False)
-        testData = fashion_mnist.test_dataloader(args.test_batch_size)
-        Net = fashion_mnist.Net
-        criterion = F.cross_entropy
-
-    # create server instance
-    model0 = Net()
-    server = Server(model0, testData, criterion, device)
-    server.set_AR(args.AR)
-    if args.dataset == 'cifar':
-        #server.set_AR_param(dbscan_eps=35.) # OK for untargeted attack, but does not detect potential targeted attack
-        server.set_AR_param(dbscan_eps=18, min_samples=12) #20 is still OK without sign-flipping, 30. is OK without sign-flipping, min_samples=10 is failed.
-    elif args.dataset == 'fashion_mnist':
-        server.set_AR_param(dbscan_eps=1.3, min_samples=5)
-
-    server.path_to_aggNet = args.path_to_aggNet
-    '''
-    honest clients are labeled as 1, malicious clients are labeled as 0
-    '''
-    label = torch.ones(args.num_clients)
-    for i in args.attacker_list_labelFlipping:
-        label[i] = 0
-    for i in args.attacker_list_labelFlippingDirectional:
-        label[i] = 0
-    for i in args.attacker_list_omniscient:
-        label[i] = 0
-    for i in args.attacker_list_backdoor:
-        label[i] = 0
-    for i in args.attacker_list_semanticBackdoor:
-        label[i] = 0
-    for i in args.list_uatk_add_noise:
-        label[i] = 0
-    for i in args.list_uatk_flip_sign:
-        label[i] = 0
-    for i in args.list_tatk_multi_label_flipping:
-        label[i] = 0
-    for i in args.list_tatk_label_flipping:
-        label[i] = 0
-    for i in args.list_tatk_backdoor:
-        label[i] = 0
-    logging.info("[1-normal, 0-malicious] label ={}".format(label))
-
-    if args.save_model_weights:
-        server.isSaveChanges = True
-        server.savePath = f'./AggData/{args.loader_type}/{args.dataset}/{args.attacks}/{args.AR}'
-        from pathlib import Path
-        Path(server.savePath).mkdir(parents=True, exist_ok=True)
-        torch.save(label, f'{server.savePath}/label.pt')
-    # create clients instance
-
-    attacker_list_labelFlipping = args.attacker_list_labelFlipping
-    attacker_list_omniscient = args.attacker_list_omniscient
-    attacker_list_backdoor = args.attacker_list_backdoor
-    attacker_list_labelFlippingDirectional = args.attacker_list_labelFlippingDirectional
-    attacker_list_semanticBackdoor = args.attacker_list_semanticBackdoor
-    for i in range(args.num_clients):
-        model = Net()
-        if args.optimizer == 'SGD':
-            optimizer = optim.SGD(model.parameters(), lr=args.lr,
-                momentum=args.momentum, weight_decay=args.weight_decay)
-        elif args.optimizer == 'Adam':
-            optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        elif args.optimizer == 'AdamW':
-            optimizer = optim.AdamW(model.parameters(), lr=args.lr)
-
-        if i in attacker_list_labelFlipping:
-            client_i = Attacker_LabelFlipping01swap(i, model, trainData[i],
-                optimizer, criterion, device, args.inner_epochs)
-        elif i in attacker_list_labelFlippingDirectional:
-            client_i = Attacker_LabelFlipping1to7(i, model, trainData[i],
-                optimizer, criterion, device, args.inner_epochs)
-        elif i in attacker_list_omniscient:
-            client_i = Attacker_Omniscient(i, model, trainData[i], optimizer,
-                criterion, device, args.omniscient_scale, args.inner_epochs)
-        elif i in attacker_list_backdoor or i in args.list_tatk_backdoor:
-            client_i = Attacker_Backdoor(i, model, trainData[i], optimizer,
-                criterion, device, args.inner_epochs)
-
-            if 'RANDOM' in args.attacks.upper():
-                client_i.utils.setRandomTrigger(seed=args.attacks)
-                logging.info(client_i.utils.trigger_position)
-                logging.info(f'Client {i} is using a random backdoor with seed \"{args.attacks}\"')
-            if 'CUSTOM' in args.attacks.upper():
-                client_i.utils.setTrigger(*args.backdoor_trigger)
-                logging.info(client_i.utils.trigger_position)
-                logging.info(f'Client {i} is using a backdoor with hyperparameter \"{args.backdoor_trigger}\"')
-
-        elif i in attacker_list_semanticBackdoor:
-            client_i = Attacker_SemanticBackdoor(i, model, trainData[i],
-                optimizer, criterion, device, args.inner_epochs)
-        elif i in args.list_tatk_label_flipping:
-            client_i = Attacker_LabelFlipping1to7(i, model, trainData[i],
-                optimizer, criterion, device, args.inner_epochs, args.source_labels[0], args.target_label)
-        elif i in args.list_tatk_multi_label_flipping:
-            client_i = Attacker_MultiLabelFlipping(i, model, trainData[i],
-                optimizer, criterion, device, args.inner_epochs, args.source_labels, args.target_label)
-        elif i in args.list_uatk_flip_sign:
-            client_i = Attacker_Omniscient(i, model, trainData[i], optimizer,
-                criterion, device, args.omniscient_scale, args.inner_epochs)
-        elif i in args.list_uatk_add_noise:
-            client_i = Attacker_AddNoise_Grad(i, model, trainData[i], optimizer,
-                criterion, device, args.mean_add_noise, args.std_add_noise,
-                args.inner_epochs)
-        elif i in args.list_unreliable:
-            channels = 3 if "cifar" in args.dataset else 1
-            kernel_size=5 if 'fashion_mnist' in args.dataset else 7
-
-            client_i = Unreliable_client(i, model, trainData[i], optimizer,
-                criterion, device, args.mean_unreliable, args.max_std_unreliable,
-                args.unreliable_fraction, args.unreliable_fracTrain,
-                args.blur_method, args.inner_epochs, channels=channels,
-                kernel_size=kernel_size)
-        else:
-            client_i = Client(i, model, trainData[i], optimizer, criterion,
-                device, args.inner_epochs)
-        server.attach(client_i)
-
-    server.set_log_path(log_dir, args.experiment_name, start_time)
-
-    loss, accuracy = server.test()
-    steps = 0
-    writer.add_scalar('test/loss', loss, steps)
-    writer.add_scalar('test/accuracy', accuracy, steps)
-
-    if args.attacks and 'BACKDOOR' in args.attacks.upper():
-        if 'SEMANTIC' in args.attacks.upper():
-            loss, accuracy, bdata, bpred = server.test_semanticBackdoor()
-        else:
-            loss, accuracy = server.test_backdoor()
-
-        writer.add_scalar('test/loss_backdoor', loss, steps)
-        writer.add_scalar('test/backdoor_success_rate', accuracy, steps)
-
-    for j in range(args.epochs):
-        steps = j + 1
-
-        logging.info('########EPOCH %d ########' % j)
-        logging.info('###Model distribution###')
-        server.distribute()
-        #         group=Random().sample(range(5),1)
-        group = range(args.num_clients)
-        server.train(group)
-        #         server.train_concurrent(group)
-
-        loss, accuracy = server.test()
-
-        writer.add_scalar('test/loss', loss, steps)
-        writer.add_scalar('test/accuracy', accuracy, steps)
-
-        if args.attacks and 'BACKDOOR' in args.attacks.upper():
-            if 'SEMANTIC' in args.attacks.upper():
-                loss, accuracy, bdata, bpred = server.test_semanticBackdoor()
-            else:
-                loss, accuracy = server.test_backdoor()
-
-            writer.add_scalar('test/loss_backdoor', loss, steps)
-            writer.add_scalar('test/backdoor_success_rate', accuracy, steps)
-
-    server.close()
-    writer.close()
